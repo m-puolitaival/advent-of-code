5a851188da3b1bd5adce1b32f0db122595f39a50
Matti Puolitaival <matti.puolitaival@tokmanni.fi>
2023-12-02 18:19:32 +0200

    tidying up day2 code

--- 2023/day2/day2.py
+++ 2023/day2/day2.py
@@ -13,10 +13,10 @@ def part1(input):
   for line in input:
     is_valid = True
     game_id = line.split(":")[0].strip("Game ")
-    for round in line.split(":")[1].split(";"):
-      for cube in round.split(","):
-        number = cube.strip().split(" ")[0]
-        color = cube.strip().split(" ")[1]
+    for round in line.strip().split(": ")[1].split("; "):
+      for cube in round.split(", "):
+        number = cube.split(" ")[0]
+        color = cube.split(" ")[1]
         if cubes[color] < int(number):
           is_valid = False
     if is_valid:
@@ -32,10 +32,10 @@ def part2(input):
       "green": 0,
       "blue": 0,
     }
-    for round in line.split(":")[1].split(";"):
-      for cube in round.split(","):
-        number = cube.strip().split(" ")[0]
-        color = cube.strip().split(" ")[1]
+    for round in line.strip().split(": ")[1].split("; "):
+      for cube in round.split(", "):
+        number = cube.split(" ")[0]
+        color = cube.split(" ")[1]
         if round_cubes[color] < int(number):
           round_cubes[color] = int(number)
     sum += round_cubes["red"] * round_cubes["green"] * round_cubes["blue"]
