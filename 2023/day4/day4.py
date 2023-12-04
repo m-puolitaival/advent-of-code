26c6ba24e1e1bd5e3b379477694b0b10f147a5a4
Matti Puolitaival <matti.puolitaival@tokmanni.fi>
2023-12-04 10:40:33 +0200

    day4 done

---
+++ 2023/day4/day4.py
@@ -0,0 +1,35 @@
+import sys
+from collections import defaultdict
+
+f = open("input.txt", "rt").read().split("\n")
+
+def part1(input):
+  total_sum = 0
+  for line in input:
+    win_nums, nums = line.split(": ")[1].split(" | ")
+    count = sum(w in nums.split() for w in win_nums.split())
+    total_sum += 2**(count-1) if count else 0
+  return total_sum
+
+def part2(input):
+  rounds = 0
+  cards = defaultdict(list)
+  for line in input:
+    cards[line.split(":")[0].split()[1]] = {
+      "num_of_cards": 1,
+      "win_nums": line.split(": ")[1].split(" | ")[0].split(),
+      "nums": line.split(": ")[1].split(" | ")[1].split()
+    }
+  for card in cards:
+    for _ in range(cards[card]["num_of_cards"]):
+      rounds += 1
+      match_count = sum(w in cards[card]["nums"] for w in cards[card]["win_nums"])
+      for wn in range(int(card)+1, int(card)+match_count+1):
+        cards[str(wn)]["num_of_cards"] += 1
+  return rounds
+
+result1 = part1(f)
+result2 = part2(f)
+
+print("Part 1 result:", result1)
+print("Part 2 result:", result2)
